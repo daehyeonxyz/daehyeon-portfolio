8cfa749cfb39c8e6d0af03f85c2a5cfe859221e3
daehyeonxyz <daehyeon.xyz@gmail.com>
2025-09-26 16:26:31 +0900

    v250926_FirstPublish

--- next.config.ts
+++ next.config.ts
@@ -2,11 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  experimental: {
-    turbo: {
-      root: "C:/Users/user/daehyeon-portfolio"
-    }
-  }
 };
 
 export default nextConfig;
